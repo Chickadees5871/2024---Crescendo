39d98d76ba660750e77739e05172886e84a5bfac
FRCChickadees-5871 <97808509+FRCChickadees-5871@users.noreply.github.com>
2024-02-24 11:01:38 -0700

    done

--- src/main/java/frc/robot/subsystems/ColorDetect.java
+++ src/main/java/frc/robot/subsystems/ColorDetect.java
@@ -1,40 +1,28 @@
-package frc.robot.subsystems;
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project
 
+package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
-
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.ColorMatchResult;
-
 import java.io.Console;
-
 import com.revrobotics.ColorMatch;
 
-public class ColorDetect {
+public class ColorDetect extends SubsystemBase{
 
-     private final I2C.Port i2cPort = I2C.Port.kOnboard;
-
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-
   private final ColorMatch m_colorMatcher = new ColorMatch();
-
-  private final Color kBlueTarget = new Color(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = new Color(0.197, 0.561, 0.240);
   private final Color kRedTarget = new Color(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = new Color(0.361, 0.524, 0.113);
 
   public void matching_Color() {
 
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
     m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget);  
 
   }
 
@@ -47,17 +35,11 @@ public class ColorDetect {
 
     if (match.color == kRedTarget) {
       colorString = "Red";
-    } else if (match.color == kGreenTarget) {
-      colorString = "Green";
-    } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
     } else {
       colorString = "Unknown";
     }
 
     System.out.println("Red-" + detectedColor.red);
-    System.out.println("Green-" + detectedColor.green);
-    System.out.println("Blue-" + detectedColor.blue);
     System.out.println("Confidence-" + match.confidence);
     System.out.println("Detected Color-" + colorString);
   }
